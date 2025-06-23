04ef2f4c664b3b38693256a173430cc2d5467daa
Jxakx <lopezjoaquin488@gmail.com>
2025-06-23 18:49:26 -0300

    Lore

---
+++ Assets/Scripts/Interactables/Nota1Interactuable.cs
@@ -0,0 +1,54 @@
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class Nota1Interactuable : MonoBehaviour
+{
+    public GameObject interactTextUI; // Texto "Presiona E para interactuar"
+    public GameObject lorePanelUI;    // Panel con el texto del diario
+    public TextMeshProUGUI loreText;  // Texto del diario
+
+    [TextArea(5, 15)]
+    public string diaryEntry; // Entrada personalizada de diario
+
+    private bool isPlayerInRange = false;
+
+    void Start()
+    {
+        interactTextUI.SetActive(false);
+        lorePanelUI.SetActive(false);
+    }
+
+    void Update()
+    {
+        if (isPlayerInRange && Input.GetKeyDown(KeyCode.E))
+        {
+            lorePanelUI.SetActive(true);
+            loreText.text = diaryEntry;
+        }
+
+        if (lorePanelUI.activeSelf && Input.GetKeyDown(KeyCode.Escape))
+        {
+            lorePanelUI.SetActive(false);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            isPlayerInRange = true;
+            interactTextUI.SetActive(true);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            isPlayerInRange = false;
+            interactTextUI.SetActive(false);
+            lorePanelUI.SetActive(false);
+        }
+    }
+}
